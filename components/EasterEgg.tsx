c43aefabbb4339778040fd9bf764fe8f90ae3379
GaloszJakub <kubagalosz@gmail.com>
2026-05-14 13:53:49 +0200

    img fix

--- components/EasterEgg.tsx
+++ components/EasterEgg.tsx
@@ -3,11 +3,12 @@ import { useEasterEgg } from '../hooks/useEasterEgg';
 
 export const EasterEgg: React.FC = () => {
   const [show, dismiss] = useEasterEgg();
+  const base = import.meta.env.BASE_URL;
 
   return (
     <div className={`egg-overlay${show ? ' show' : ''}`} onClick={dismiss}>
       <img
-        src="/IMG_20260119_110323751.gif"
+        src={`${base}IMG_20260119_110323751.gif`}
         alt="Surprise cat"
         className="egg-img"
       />
